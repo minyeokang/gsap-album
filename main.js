f9e2537c287414774845e76123a371f8d4ab483c
minyeokang <dobieborie1@gmail.com>
2023-04-12 16:01:30 +0900

    add basic scrolltrigger

--- main.js
+++ main.js
@@ -2,24 +2,44 @@
 //2. track offsetTop hit the trigger, background color turns white
 
 // gsap.to(".img-wrap", {
-//     scrollTrigger: ".wrap", // start the animation when ".box" enters the viewport (once)
-//     x: 500,
-//     markers: true
+//     rotate: "360deg",
+//     duration: 10,
+//     repeat: -1
 //   });
 
-//the rotating supposed to start in the beginnig
+
+
 let tl = gsap.timeline()
-tl.to('.img-wrap',{rotate: "360deg"})
-  ScrollTrigger.create({
-    trigger: "body",
-    markers: true,
-    start: "top top",
-    animation: tl,
-    scrub: 1,
-    end: "+=100%",
-    onToggle: self => console.log("toggled, isActive:", self.isActive),
-    onUpdate: self => {
-      console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
-    }
-  });
-  
+
+tl.to('.img-wrap', {
+  duration: 10,
+  rotate: "360deg",
+});
+
+ScrollTrigger.create({
+  trigger: "body",
+  markers: true,
+  start: "top top",
+  animation: tl,
+  scrub: 1,
+  end: "+=100%",
+  // onEnter: ()=>{tl.play()}
+});
+
+
+let tl2 = gsap.timeline()
+
+tl2.to('body',{
+  background: "#ffffff",
+  color: "#000000"
+})
+
+ScrollTrigger.create({
+  trigger: ".trigger",
+  markers: true,
+  start: "top top",
+  animation: tl2,
+  scrub: 1,
+  end: "bottom bottom",
+  // onEnter: ()=>{tl.play()}
+});
